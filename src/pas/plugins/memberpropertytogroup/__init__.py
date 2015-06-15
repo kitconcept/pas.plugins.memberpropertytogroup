03bdf708abbfae63bbaac9886ae9fad486843544
Timo Stollenwerk <tisto@plone.org>
2015-06-15 19:08:00 +0200

    Plone 3 compatibility.

--- src/pas/plugins/memberpropertytogroup/__init__.py
+++ src/pas/plugins/memberpropertytogroup/__init__.py
@@ -4,3 +4,14 @@
 from zope.i18nmessageid import MessageFactory
 
 _ = MessageFactory('pas.plugins.memberpropertytogroup')
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product.
+
+    This is referenced from configure.zcml. Regstrations as a "Zope 2 product"
+    is necessary for GenericSetup profiles to work, for example.
+
+    Here, we call the Archetypes machinery to register our content types
+    with Zope and the CMF.
+    """
