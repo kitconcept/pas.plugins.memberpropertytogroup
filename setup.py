327bdb641af9616327a5b1a32793353501f2c412
Jens W. Klein <jk@kleinundpartner.at>
2015-07-18 00:30:49 +0200

    need to depend on Plone, because Products.CMFPlone has no CMFPlacefulWorklfow

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'Products.CMFPlone',
+        'Plone',
         'setuptools',
     ],
     extras_require={
