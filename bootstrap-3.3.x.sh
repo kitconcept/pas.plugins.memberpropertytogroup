0696bc70733d1eb77748b273c0c6d14149f3dec2
Jens W. Klein <jk@kleinundpartner.at>
2015-07-18 00:32:13 +0200

    force symlink

--- bootstrap-3.3.x.sh
+++ bootstrap-3.3.x.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ln -s plone-3.3.x.cfg buildout.cfg
+ln -fs plone-3.3.x.cfg buildout.cfg
 python2.4 plone-3.3.x-bootstrap.py -v 1.4.4
 mkdir -p downloads
-./bin/buildout 
+./bin/buildout
--- bootstrap-5.0.x.sh
+++ bootstrap-5.0.x.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # see https://community.plone.org/t/not-using-bootstrap-py-as-default/620
-ln -s plone-5.0.x.cfg buildout.cfg
+ln -fs plone-5.0.x.cfg buildout.cfg
 rm -r ./lib ./include ./local ./bin
 virtualenv --clear .
-./bin/pip install --upgrade pip setuptools zc.buildout 
-./bin/buildout 
+./bin/pip install --upgrade pip setuptools zc.buildout
+./bin/buildout
