56a4b544d4fca3fd80ee90be005cc335c33fe357
Érico Andrei <ericof@gmail.com>
2021-08-31 16:22:07 +0200

    Back to development: 2.1.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ long_description = (
 
 setup(
     name="pas.plugins.memberpropertytogroup",
-    version="2.1.0",
+    version="2.1.1.dev0",
     description=description,
     long_description=long_description,
     classifiers=[
