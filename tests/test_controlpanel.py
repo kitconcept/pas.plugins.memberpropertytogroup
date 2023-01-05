425d59381f0dd5cbf3705913aad8deaa23a12747
Érico Andrei <ericof@gmail.com>
2023-01-05 12:12:34 -0300

    Use class-based tests (which seem to speed up testing)

--- tests/test_controlpanel.py
+++ tests/test_controlpanel.py
@@ -20,47 +20,43 @@ def registry(portal):
     return registry
 
 
-def test_validator(portal):
-    from zope.interface import Invalid
+class TestControlPanel:
+    def test_validator(self, portal):
+        from zope.interface import Invalid
 
-    with pytest.raises(Invalid):
-        ifaces.validate_valid_groups(["1"])
-    with pytest.raises(Invalid):
-        ifaces.validate_valid_groups(["1|2|3|4|5|6"])
+        with pytest.raises(Invalid):
+            ifaces.validate_valid_groups(["1"])
+        with pytest.raises(Invalid):
+            ifaces.validate_valid_groups(["1|2|3|4|5|6"])
 
+    def test_memberpropertytogroup_controlpanel_view(self, portal, http_request):
+        view = api.content.get_view(
+            "memberpropertytogroup-controlpanel", portal, http_request
+        )
+        assert view is not None
 
-def test_memberpropertytogroup_controlpanel_view(portal, http_request):
-    view = api.content.get_view(
-        "memberpropertytogroup-controlpanel", portal, http_request
-    )
-    assert view is not None
+    def test_memberpropertytogroup_controlpanel_view_protected(self, anon_portal):
+        with pytest.raises(Unauthorized):
+            anon_portal.restrictedTraverse("@@memberpropertytogroup-controlpanel")
 
+    def test_memberpropertytogroup_in_controlpanel(self, portal):
+        controlpanel = api.portal.get_tool("portal_controlpanel")
+        assert "memberpropertytogroup" in [
+            a.getAction(portal)["id"] for a in controlpanel.listActions()
+        ]
 
-def test_memberpropertytogroup_controlpanel_view_protected(anon_portal):
-    with pytest.raises(Unauthorized):
-        anon_portal.restrictedTraverse("@@memberpropertytogroup-controlpanel")
+    def test_record_group_property(self, registry):
+        record = registry.records[
+            "pas.plugins.memberpropertytogroup.interfaces."
+            "IPasPluginsMemberpropertytogroupSettings.group_property"
+        ]
+        assert "group_property" in ifaces.IPasPluginsMemberpropertytogroupSettings
+        assert record.value == ""
 
-
-def test_memberpropertytogroup_in_controlpanel(portal):
-    controlpanel = api.portal.get_tool("portal_controlpanel")
-    assert "memberpropertytogroup" in [
-        a.getAction(portal)["id"] for a in controlpanel.listActions()
-    ]
-
-
-def test_record_group_property(registry):
-    record = registry.records[
-        "pas.plugins.memberpropertytogroup.interfaces."
-        "IPasPluginsMemberpropertytogroupSettings.group_property"
-    ]
-    assert "group_property" in ifaces.IPasPluginsMemberpropertytogroupSettings
-    assert record.value == ""
-
-
-def test_record_valid_groups(registry):
-    record = registry.records[
-        "pas.plugins.memberpropertytogroup.interfaces."
-        "IPasPluginsMemberpropertytogroupSettings.valid_groups"
-    ]
-    assert "valid_groups" in ifaces.IPasPluginsMemberpropertytogroupSettings
-    assert record.value == []
+    def test_record_valid_groups(self, registry):
+        record = registry.records[
+            "pas.plugins.memberpropertytogroup.interfaces."
+            "IPasPluginsMemberpropertytogroupSettings.valid_groups"
+        ]
+        assert "valid_groups" in ifaces.IPasPluginsMemberpropertytogroupSettings
+        assert record.value == []
